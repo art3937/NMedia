6e823d32264fd9eec582931d8d477c0b046402da
Artemii Telegin <art3937@gmail.com>
2025-04-20 19:13:25 +0300

    fragment save error

--- app/src/main/java/ru/netology/nmedia/activity/NewPostFragment.kt
+++ app/src/main/java/ru/netology/nmedia/activity/NewPostFragment.kt
@@ -43,7 +43,7 @@ class NewPostFragment() : Fragment() {
             }
             findNavController().navigateUp()
         }
-        viewModel.cancel()
+       // viewModel.cancel()
         return binding.root
     }
 
